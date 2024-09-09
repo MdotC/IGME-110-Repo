732b4353f1af4a3aad806d6e42cd6696055e9c07
mpc6024 <mpc6024@OH1380-26>
2024-09-09 11:34:07 -0400

    Add project files.

---
+++ HelloGDAPSTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloGDAPSTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
